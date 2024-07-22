8565784e5eafcb70dee854829a91b32f93ed8ddd
Marcos-Antunes04 <mca4729@gmail.com>
2024-07-22 14:05:54 -0300

    Checksum conditional assignment

--- checksum.vhd
+++ checksum.vhd
@@ -21,29 +21,23 @@ begin
     variable check_intermed : std_logic_vector(15 downto 0) := (others=>'0');
 
     begin
-        case i_counter is
-            when X"0000" =>
+        if(i_counter = X"0000") then
                 check_intermed := (others => '0');
+                received_checksum <= (others => '0');
                 check_value <= (others => '0');
-            when X"0001" =>
+        elsif(((to_integer(unsigned(i_counter)) mod 2) = 1) and  not (to_integer(unsigned(i_counter)) = 3)) then
                 check_intermed := X"00" & i_data;
-            when X"0002" =>
+        elsif(((to_integer(unsigned(i_counter)) mod 2) = 0) and  not (to_integer(unsigned(i_counter)) = 4)) then
                 check_intermed := check_intermed(7 downto 0) & i_data;
                 check_value <= std_logic_vector(unsigned(check_value) + unsigned(check_intermed));
                 check_intermed := (others => '0');
-            when X"0003" =>
-                received_checksum <= X"00" & i_data;
-            when X"0004" =>
-                received_checksum <= received_checksum(7 downto 0) & i_data;
-            when X"0005" =>
-                check_intermed := X"00" & i_data;
-            when X"0006" =>
-                check_intermed := check_intermed(7 downto 0) & i_data;
-                check_value <= std_logic_vector(unsigned(check_value) + unsigned(check_intermed));
-                check_intermed := (others => '0');
-
-            when others => null;
-        end case;
+        end if;
+        
+        if(to_integer(unsigned(i_counter)) = 3) then
+            received_checksum <= X"00" & i_data;
+        elsif(to_integer(unsigned(i_counter)) = 4) then
+            received_checksum <= received_checksum(7 downto 0) & i_data;
+        end if;
     end process;
 
 end behavioral;
